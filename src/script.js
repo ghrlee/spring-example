a8bbf01446e4f64de4fade1d85e42bd84df30321
George Lee <ghrlee@gmail.com>
2022-12-08 13:38:08 -0600

    adding x + y commented out

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ import p5 from "p5"
 let s = (sk) => {
   let yPosition = 300;
   let restLength = 200;
-  let springStrength = .033;
+  let springStrength = .045;
   let velocity = 0;
   
   sk.setup = () =>{
@@ -22,8 +22,43 @@ let s = (sk) => {
 
     velocity *= .92
   }
-  
-
 }
+
+// let s = (sk) => {
+//   var mousePos,
+//     pos,
+//     target,
+//     vel,
+//     drag;
+    
+//   sk.setup = () =>{
+//     sk.createCanvas(600, 400);
+    
+//     mousePos = sk.createVector(0,0);
+//     pos = sk.createVector(0,0);
+//     target = sk.createVector(0,0);
+//     vel = sk.createVector(0,0);
+    
+//     drag = 0.76; //need to take some force away, 1 = no drag
+//     sk.strength = 0.15; // the "strength" of the spring, out of 1
+    
+//     sk.r = 40; 
+//   }
+  
+//   sk.draw = () => {
+//     sk.background(112, 200, 126);
+//     mousePos.set(sk.mouseX, sk.mouseY);
+//     target = mousePos;
+
+//     var force = p5.Vector.sub(target, pos);
+//     force = force.mult(sk.strength);
+//     vel = vel.mult(drag);
+//     vel = vel.add(force);  
+//     pos = pos.add(vel);
+    
+//     sk.fill(45, 197, 244);
+//     sk.ellipse(pos.x, pos.y, sk.r);
+//   }
+// }
 
 const P5 = new p5(s);
